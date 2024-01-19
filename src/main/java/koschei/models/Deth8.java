7cc0831dd2381c3b67f6bf46c1e509ee50227fe9
zenger05 <zenger05@yandex.ru>
2024-01-20 02:42:51 +0300

    first

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "";
+    }
 }
